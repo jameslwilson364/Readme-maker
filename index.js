00fc82d6b7447dfc533b5911e02d279446886c08
Jimmy Wilson <james.l.wilson364@gmail.com>
2023-10-21 14:29:18 -0500

    saturday afternoon only one thing left

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
-// license gives a list of options exercise 20; also grab the list of options from github and adds a badge
-
 // link the table of contents to the corresponding sections of the README
 
+// this function takes the user's input and then writes the READMEGEN file
 function writeFile() {
 inquirer
   .prompt([
@@ -60,6 +58,9 @@ inquirer
   ])
   .then((response) => {
     console.log(response.projectName);
+    const licenseChoice = response.projectLicense;
+    console.log(licenseChoice);
+    if (licenseChoice == 'GNU General Public License v3.0') {
     const userName = `# ${response.projectName}
     
 - Description: ${response.projectDescription}
@@ -79,30 +80,150 @@ inquirer
 - ${response.projectUsage}
 
 ## Contribution Information
-- ${response.Cont}
+- ${response.projectCont}
 
 ## Test Instructions
 - ${response.projectTest}
 
 ## License Information
-- ${response.projectLicense}
+- ${response.projectLicense} ![](./assets/GNU.svg)
 
 ## Questions?
 - https://github.com/${response.projectGitHub}
 - ${response.projectEmail}
 
 ## End File`;
+    
+    // writes to the new file
     fs.writeFile('READMEGEN.md', `${userName}`, (err) =>
     err ? console.error(err) : console.log('README logged!')
     );
     
-  });
-}
+    
+  } else if (licenseChoice == 'MIT License') {
+    const userName = `# ${response.projectName}
+    
+- Description: ${response.projectDescription}
 
+## Table of Contents
+1. Installation Instructions
+2. Usage Information
+3. Contribution Information
+4. Test Instructions
+5. License Information
+6. Questions and Contact Information
+
+## Installation Instructions
+- ${response.projectInstallation}
+
+## Usage Information
+- ${response.projectUsage}
+
+## Contribution Information
+- ${response.projectCont}
+
+## Test Instructions
+- ${response.projectTest}
+
+## License Information
+- ${response.projectLicense} ![](./assets/MIT.svg)
+
+## Questions?
+- https://github.com/${response.projectGitHub}
+- ${response.projectEmail}
+
+## End File`;
+    
+    // writes to the new file
+    fs.writeFile('READMEGEN.md', `${userName}`, (err) =>
+    err ? console.error(err) : console.log('README logged!')
+    );
+  } else if (licenseChoice == 'Mozilla Public License 2.0' ) { const userName = `# ${response.projectName}
+    
+  - Description: ${response.projectDescription}
+  
+  ## Table of Contents
+  1. Installation Instructions
+  2. Usage Information
+  3. Contribution Information
+  4. Test Instructions
+  5. License Information
+  6. Questions and Contact Information
+  
+  ## Installation Instructions
+  - ${response.projectInstallation}
+  
+  ## Usage Information
+  - ${response.projectUsage}
+  
+  ## Contribution Information
+  - ${response.projectCont}
+  
+  ## Test Instructions
+  - ${response.projectTest}
+  
+  ## License Information
+  - ${response.projectLicense} ![](./assets/Mozilla.svg)
+  
+  ## Questions?
+  - https://github.com/${response.projectGitHub}
+  - ${response.projectEmail}
+  
+  ## End File`;
+      
+      // writes to the new file
+      fs.writeFile('READMEGEN.md', `${userName}`, (err) =>
+      err ? console.error(err) : console.log('README logged!')
+      );
+
+  } else if (licenseChoice == 'The Unlicense') {
+    const userName = `# ${response.projectName}
+    
+- Description: ${response.projectDescription}
+
+## Table of Contents
+1. Installation Instructions
+2. Usage Information
+3. Contribution Information
+4. Test Instructions
+5. License Information
+6. Questions and Contact Information
+
+## Installation Instructions
+- ${response.projectInstallation}
+
+## Usage Information
+- ${response.projectUsage}
+
+## Contribution Information
+- ${response.projectCont}
+
+## Test Instructions
+- ${response.projectTest}
+
+## License Information
+- ${response.projectLicense} ![](./assets/Unlicense.svg)
+
+## Questions?
+- https://github.com/${response.projectGitHub}
+- ${response.projectEmail}
+
+## End File`;
+    
+    // writes to the new file
+    fs.writeFile('READMEGEN.md', `${userName}`, (err) =>
+    err ? console.error(err) : console.log('README logged!')
+    );
+  } else {
+    console.log("You failed to pick a license");
+  }
+});
+} 
 
 function init() {
   writeFile();
 }
 
-
 init();
+
+
